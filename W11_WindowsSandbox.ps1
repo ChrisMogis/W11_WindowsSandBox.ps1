07971a4ae117de628a6b464e2266d46b203f1f91
Christopher Mogis <102024007+ChrisMogis@users.noreply.github.com>
2022-09-30 10:41:55 +0200

    Update W11_WindowsSandbox.ps1

--- W11_WindowsSandbox.ps1
+++ W11_WindowsSandbox.ps1
@@ -12,7 +12,8 @@ $ComputerInfo = Get-ComputerInfo
 
         Write-Output ""
         $continue = $true
-        while ($continue){
+        while ($continue)
+        {
           write-host "---------------------- Windows SandBox Installation -----------------------"
           write-host "1. Prerequisites check" -ForegroundColor Cyan
           write-host "2. Install Windows Sandbox" -ForegroundColor Cyan
@@ -20,70 +21,74 @@ $ComputerInfo = Get-ComputerInfo
           write-host "---------------------------------------------------------------------------"
           $choix = read-host "Choose an action :"
           switch ($choix){
+            #Prerequisites for used Windows SandBox
             "1" {
-                #Prerequisites for used Windows SandBox
                 #Windows Version
                 Write-Output ""
-                Write-Output  "### Windows SandBox prerequisites check ###"
+                Write-Output  "### Check Operating System ###"
                 Write-Output ""
                 $WinVer = $ComputerInfo.WindowsProductName
                 if($WinVer -eq "Windows 10 Enterprise" -OR "Windows 10 Professional" )
-                {
-                    Write-Host "$WinVer installed on $($ComputerInfo.CsName) is compatible with the Windows Sandbox feature" -ForegroundColor Green <# Action to perform if the condition is true #>
-                }
+                    {
+                    Write-Host "Your Operating system is compatible with Windows Sandbox" -ForegroundColor Green <# Action to perform if the condition is true #>
+                    Write-Host "Windows version installed : $Winver" -ForegroundColor Yellow
+                    }
                 else 
-                {
-                    Write-Host "$WinVer installed on $($ComputerInfo.CsName) is not compatible with the Windows Sandbox feature" -ForegroundColor Red <# Action when all if and elseif conditions are false #>
-                }
+                    {
+                    Write-Host "$WinVer installed on $($ComputerInfo.CsName) is not compatible with Windows Sandbox" -ForegroundColor Red <# Action when all if and elseif conditions are false #>
+                    }
+
+                #Virtual Machine
+                $CoreCPU = $ComputerInfo.CsSystemFamily
+                if($CoreCPU -ge "Virtual Machine"  )
+                    {
+                Write-Output ""
+                Write-Output  "### Check Nested virtualization for Virtual Machine ###"
+                Write-Output ""
+                Write-Host "For used Windows Sandbox on virtual michine, please check if nested virtualization is activated" -ForegroundColor Yellow <# Action to perform if the condition is true #>
+                    }
+                
                 #CPU Architecture
                 Write-Output ""
                 Write-Output  "### Check CPU Configuration ###"
                 Write-Output ""
                 $CoreCPU = $ComputerInfo.CsNumberOfLogicalProcessors
                 if($CoreCPU -ge "3"  )
-                {
-                    Write-Host "The CPU on $($ComputerInfo.CsName) is compatible with the Windows Sandbox feature" -ForegroundColor Green <# Action to perform if the condition is true #>
-                }
+                    {
+                    Write-Host "The CPU on $($ComputerInfo.CsName) is compatible with Windows Sandbox" -ForegroundColor Green <# Action to perform if the condition is true #>
+                    Write-Host "Number of Core : $CoreCPU" -ForegroundColor Yellow
+                    }
                 else 
-                {
-                    Write-Host "The CPU on $($ComputerInfo.CsName) is not compatible with the Windows Sandbox feature" -ForegroundColor Red <# Action when all if and elseif conditions are false #>
-                }
+                    {
+                    Write-Host "The CPU on $($ComputerInfo.CsName) is not compatible with Windows Sandbox" -ForegroundColor Red <# Action when all if and elseif conditions are false #>
+                    Write-Host "Number of Core : $CoreCPU. Microsoft recommends having a processor with at least 4 cores" -ForegroundColor Yellow
+                    }
+
                 #RAM Available
                 Write-Output ""
                 Write-Output  "### Check Memory Ram Installed ###"
                 Write-Output ""
-                $RamMemory = Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | ForEach-Object {"{0:N2}" -f ([math]::round($_.Sum / 1GB))}
-                if($RamMemory -lt "4.00")
-                {
-                    Write-Host "The Quantity of Memory Ram installed on $($ComputerInfo.CsName) is ready  with the Windows Sandbox feature" -ForegroundColor Green <# Action to perform if the condition is true #>
-                }
+                $RamMemory = $ComputerInfo.CsTotalPhysicalMemory
+                if($RamMemory -gt "4222190592")
+                    {
+                    Write-Host "The Quantity of Memory Ram installed on $($ComputerInfo.CsName) is ready with Windows Sandbox" -ForegroundColor Green <# Action to perform if the condition is true #>
+                    }
                 else 
-                {
-                    Write-Host "The Quantity of Memory Ram installed on $($ComputerInfo.CsName) is not ready with the Windows Sandbox feature" -ForegroundColor Red <# Action when all if and elseif conditions are false #>
-                }
-                #RAM Available
-                Write-Output ""
-                Write-Output  "### Check HyperV feature ###"
-                Write-Output ""
-                $HyperV = (Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online).State
-                if($HyperV -eq "Enabled")
-                {
-                    Write-Host "Windows Hypervisor is enabled on your computer" -ForegroundColor Green <# Action to perform if the condition is true #>
-                }
-                else 
-                {
-                    Write-Host "WARNING - Windows Hypervisor is not enabled on your computer" -ForegroundColor Red <# Action when all if and elseif conditions are false #>
-                }
+                    {
+                    Write-Host "The amount of memory installed on your computer $($ComputerInfo.CsName) is not enough to run Windows Sandbox" -ForegroundColor Red <# Action when all if and elseif conditions are false #>
+                    Write-Host "Microsoft recommends having at least 4 GB of memory" -ForegroundColor Yellow
+                    }
                 Write-Output ""
                 }
-                #Option 3 : Windows Sandbox installation
+
+                #Option 2 : Windows Sandbox installation
             "2" {
                     Enable-WindowsOptionalFeature -FeatureName "Containers-DisposableClientVM" -All -Online
-                    Write-Host "After installation, please restart your computer and relaunch this script and select option 1 " -ForegroundColor Green
+                    Write-Host "After installation, please select Exit !" -ForegroundColor Green
                     Write-Output ""
                 }
                 #Option x : Exit menu
             "x" {$continue = $false}
-            default {Write-Host "Choix invalide" -ForegroundColor Red}
+            default {Write-Host "Invalid choice" -ForegroundColor Red}
           }
         }
